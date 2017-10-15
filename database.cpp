267181f62f86226c2c25eb73d565ec55c9258adc
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2017-10-15 21:24:38 +0900

    databaseクラスに必要な関数追加 コメント追加

--- database.cpp
+++ database.cpp
@@ -9,6 +9,7 @@ inline vector<string> database::return_komoku_x() { return komoku_x; }
 inline vector<string> database::return_komoku_y() { return komoku_y; }
 
 inline void database::write(int x, int y, string input) {
+	//指定した座標の要素を書き換える
 	if (table[0].size() < x && table.size() < y) {
 		cout << "error" << endl;
 		return;
@@ -18,13 +19,25 @@ inline void database::write(int x, int y, string input) {
 
 
 inline void database::add_komoku_x(string input) {
-
+	//項目x追加(input:項目名)
 }
 
 inline void database::add_komoku_y(string input) {
-
+	//項目y追加(input:項目名)
 }
 
 inline void database::file_output() {
+	//ファイル出力
+}
 
+inline int database::search_x(string name) {
+	//項目xから名前を検索(name:項目名)
+}
+
+inline int database::search_y(string name) {
+	//項目yから名前を検索(name:項目名)
+}
+
+inline void database::sort(bool komoku, int n, bool order) {
+	//項目xをソート(komoku:xかyか,n:vecter[n],order:昇順か降順か)
 }
--- database.hpp
+++ database.hpp
@@ -20,8 +20,12 @@ public:
 	vector<vector<string>> return_table();
 	vector<string> return_komoku_x();
 	vector<string> return_komoku_y();
+
 	void write(int x, int y, string input);//指定した座標の要素を書き換える
-	void add_komoku_x(string input);//項目x追加
-	void add_komoku_y(string input);//項目y追加
+	void add_komoku_x(string input);//項目x追加(input:項目名)
+	void add_komoku_y(string input);//項目y追加(input:項目名)
 	void file_output();//ファイル出力
+	int search_x(string name); //項目xから名前を検索(name:項目名)
+	int search_y(string name); //項目yから名前を検索(name:項目名)
+	void sort(bool komoku,int n,bool order); //項目xをソート(komoku:xかyか,n:vecter[n],order:昇順か降順か)
 };
